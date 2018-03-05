397ec62046021fe4c70c3868e84a8b8cd72679b1
javitopwn <34965674+javitopwn@users.noreply.github.com>
2018-03-05 14:18:58 +0100

    Update Signalled_2Readers_Queue.java

--- A3/Signalled_2Readers_Queue.java
+++ A3/Signalled_2Readers_Queue.java
@@ -1,12 +1,14 @@
 package concurrent_assignment2.A3;
 
 import concurrent_assignment2.A_intro.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Use the synchronized keyword and signals.
  * 
  * You cannot decide whose's turn it is based on 
- * a 2 states variables, so know use a variable which
+ * a 2 states variables, so now use a variable which
  * allows for more values (you need 3 states, that is, 3 turns).
  * 
  * Output should be: writer prints, both readers read, and so on...
@@ -14,20 +16,37 @@ import concurrent_assignment2.A_intro.Queue;
  */
  
 class Signalled_2Readers_Queue implements Queue{
-	int n=0;
+	volatile int n=0;
+        volatile int turn=0;
 	
 	
 	@Override
 	public void read(int ID) {
 		// TODO Auto-generated method stub
-		
+		if(turn==ID){ // SI ES IGUAL ID , PRINT Y LE SUMO 1 PARA QUE SEA EL TURNO DEL SIGUIENTE Y LUEGO LO PONGO A 0 PARA QUE VUELVA EL WRITER
+                    try {
+                        wait();
+                    } catch (InterruptedException ex) {
+                    }
+                }
+                System.out.println("ID:  "+ID+"  Value:"+n);
+                turn = 0;
+                notifyAll();
 	}
 	
 
 	@Override
 	public void write(int x) {
 		// TODO Auto-generated method stub
-		
+		if (turn!=0){
+                    try {
+                        wait();
+                    } catch (InterruptedException ex) {
+                    }
+                }
+                n=x;
+                turn=1;
+                notifyAll();
 	}
 	
 	@Override
@@ -40,5 +59,6 @@ class Signalled_2Readers_Queue implements Queue{
 	
 	
 }
+
 
 

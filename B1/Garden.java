b52ea3bf59999d31eae71da175de49f9dc5fade4
Viktitors <34943687+Viktitors@users.noreply.github.com>
2018-03-12 13:14:20 +0100

    Update Garden.java

--- B1/Garden.java
+++ B1/Garden.java
@@ -1,19 +1,19 @@
 
 package concurrent_assignment2.B1;
-
+ 
 /**
  * Search for the exact critical section,
  * and protect it with Dekker's algorithm.
  * */
-
+ 
 import java.awt.*;
 import java.awt.event.*;
 import java.applet.*;
 import java.applet.*;
-
-
+ 
+ 
 public class Garden extends Applet {
-
+ 
     Button goButton;
     Turnstile turnstile1;
     Turnstile turnstile2;
@@ -23,35 +23,35 @@ public class Garden extends Applet {
     NumberCanvas turn2D;
 //    Checkbox fixit;
     public final static int MAX = 20;
-
-
+ 
+ 
     public void init() {
         super.init();
-	setBackground(Color.lightGray);
-        // Set up Button 
+    setBackground(Color.lightGray);
+        // Set up Button
         Panel p0= new Panel();
         p0.add(goButton = new Button(" Simulate "));
         goButton.setFont(new Font("Helvetica",Font.BOLD,24));
-		
+       
         goButton.addActionListener(new ActionListener() {
           public void actionPerformed(ActionEvent e) {
            if (turnstile1==null && turnstile2==null)
               go();
           else if (!turnstile1.isAlive() && !turnstile2.isAlive())
-              go();         
+              go();        
           }
         });
-	Panel p=new Panel();
+    Panel p=new Panel();
         Label lb=new Label("Ornamental Garden");
         lb.setFont(new Font("Arial",Font.BOLD,24));
         p.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         p.add("South",lb);
-
+ 
         Panel p1= new Panel();
         p1.setLayout(new BorderLayout());
-        
+       
         p1.add("Center",p0);
-
+ 
         // Set up Display
         Panel p2 = new Panel();
         counterD = new NumberCanvas("Counter");
@@ -68,65 +68,81 @@ public class Garden extends Applet {
         add("North",p);
         add("Center",p2);
         add("South",p1);
-        
+       
     }
-
+ 
     private void go() {
-        
+       
         counter = new Counter(counterD);
        
-        turnstile1= new Turnstile(turn1D,counter);
-        turnstile2= new Turnstile(turn2D,counter);
+        turnstile1= new Turnstile(turn1D,counter,0);
+        turnstile2= new Turnstile(turn2D,counter,1);
         turnstile1.start();
         turnstile2.start();
     }
-
+ 
 }
-
+ 
 class Counter {
-
+    volatile int turn=0;
+    boolean[] wantsCS={false,false};
     int value=0;
     NumberCanvas display;
-
+ 
     Counter(NumberCanvas n) {
         display=n;
         display.setvalue(value);
     }
-
-    void increment() {
+ 
+    void increment(int ID) {
+       //prepotocol
+        wantsCS[ID]=true;  //if
+        while(wantsCS[1-ID]){   //while the other wants to enter
+            if(turn==1-ID){     //if it's its turn
+                wantsCS[ID]=false; //I dont want to enter
+                while(turn==1-ID); //while its its turn
+                wantsCS[ID]=true;  //next turn
+            }
+        }
+       
         int temp = value;   //read[v]
         CC.ForceCC();
         value=temp+1;       //write[v+1]
         display.setvalue(value);
+        //non-CS
+        //post-protocol
+        turn=1-ID;
+        wantsCS[ID]=false;
     }
 }
-
-
-
-
+ 
+ 
+ 
+ 
 class Turnstile extends Thread {
   NumberCanvas display;
   Counter people;
-
-  Turnstile(NumberCanvas n,Counter c)
-    { display = n; people = c; }
-
+  int ID;
+ 
+  Turnstile(NumberCanvas n,Counter c, int ID)
+    { display = n; people = c;this.ID=ID; }
+ 
   public void run() {
     try{
       display.setvalue(0);
       for (int i=1;i<=Garden.MAX;i++){
         Thread.sleep(500); //0.5 second
         display.setvalue(i);
-        people.increment();
+        people.increment(ID);
       }
     } catch (InterruptedException e) {}
   }
 }
-
+ 
 class CC {
     public static void ForceCC() {
         if (Math.random()<0.5)
            try{Thread.sleep(200);} catch(InterruptedException e){};
-            
+           
     }
 }

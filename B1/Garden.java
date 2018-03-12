67a8b141d537398a6e737581de6a835b4ce4d235
javitopwn <34965674+javitopwn@users.noreply.github.com>
2018-03-12 13:35:28 +0100

    Update Garden.java

--- B1/Garden.java
+++ B1/Garden.java
@@ -1,19 +1,19 @@
 
 package concurrent_assignment2.B1;
- 
+
 /**
  * Search for the exact critical section,
  * and protect it with Dekker's algorithm.
  * */
- 
+
 import java.awt.*;
 import java.awt.event.*;
 import java.applet.*;
 import java.applet.*;
- 
- 
+
+
 public class Garden extends Applet {
- 
+
     Button goButton;
     Turnstile turnstile1;
     Turnstile turnstile2;
@@ -23,35 +23,35 @@ public class Garden extends Applet {
     NumberCanvas turn2D;
 //    Checkbox fixit;
     public final static int MAX = 20;
- 
- 
+
+
     public void init() {
         super.init();
-    setBackground(Color.lightGray);
-        // Set up Button
+	setBackground(Color.lightGray);
+        // Set up Button 
         Panel p0= new Panel();
         p0.add(goButton = new Button(" Simulate "));
         goButton.setFont(new Font("Helvetica",Font.BOLD,24));
-       
+		
         goButton.addActionListener(new ActionListener() {
           public void actionPerformed(ActionEvent e) {
            if (turnstile1==null && turnstile2==null)
               go();
           else if (!turnstile1.isAlive() && !turnstile2.isAlive())
-              go();        
+              go();         
           }
         });
-    Panel p=new Panel();
+	Panel p=new Panel();
         Label lb=new Label("Ornamental Garden");
         lb.setFont(new Font("Arial",Font.BOLD,24));
         p.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         p.add("South",lb);
- 
+
         Panel p1= new Panel();
         p1.setLayout(new BorderLayout());
-       
+        
         p1.add("Center",p0);
- 
+
         // Set up Display
         Panel p2 = new Panel();
         counterD = new NumberCanvas("Counter");
@@ -68,11 +68,11 @@ public class Garden extends Applet {
         add("North",p);
         add("Center",p2);
         add("South",p1);
-       
+        
     }
- 
+
     private void go() {
-       
+        
         counter = new Counter(counterD);
        
         turnstile1= new Turnstile(turn1D,counter,0);
@@ -80,53 +80,53 @@ public class Garden extends Applet {
         turnstile1.start();
         turnstile2.start();
     }
- 
+
 }
- 
+
 class Counter {
     volatile int turn=0;
     boolean[] wantsCS={false,false};
     int value=0;
     NumberCanvas display;
- 
+
     Counter(NumberCanvas n) {
         display=n;
         display.setvalue(value);
     }
- 
+
     void increment(int ID) {
        //prepotocol
-        wantsCS[ID]=true;  //if
+        wantsCS[ID]=true;  //if I want to enter and I do cs and post-protocol
         while(wantsCS[1-ID]){   //while the other wants to enter
-            if(turn==1-ID){     //if it's its turn
+            if(turn==1-ID){     //if it's its turn 
                 wantsCS[ID]=false; //I dont want to enter
-                while(turn==1-ID); //while its its turn
+                while(turn==1-ID); //while its its turn 
                 wantsCS[ID]=true;  //next turn
             }
         }
-       
+        //cs 
         int temp = value;   //read[v]
         CC.ForceCC();
         value=temp+1;       //write[v+1]
         display.setvalue(value);
         //non-CS
         //post-protocol
-        turn=1-ID;
+        turn=1-ID;     // Now it's the other one's turn
         wantsCS[ID]=false;
     }
 }
- 
- 
- 
- 
+
+
+
+
 class Turnstile extends Thread {
   NumberCanvas display;
   Counter people;
   int ID;
- 
+
   Turnstile(NumberCanvas n,Counter c, int ID)
     { display = n; people = c;this.ID=ID; }
- 
+
   public void run() {
     try{
       display.setvalue(0);
@@ -138,11 +138,12 @@ class Turnstile extends Thread {
     } catch (InterruptedException e) {}
   }
 }
- 
+
 class CC {
     public static void ForceCC() {
         if (Math.random()<0.5)
            try{Thread.sleep(200);} catch(InterruptedException e){};
-           
+            
     }
 }
+
